fb460e954e9ea457520c9dc338f8b533d02dbb2a
necoline <hnhubner@gmail.com>
2016-07-28 19:34:22 -0600

    making buttons and tables

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// onclick the card flips
+//if match the image stays
+  //if match add points and adds attemp #
+//else image returns
+  //else adds attempt number
+//onclick 'new game' button
+  //clears history
+  // randomizes imgs
+
+function flip() {
+  var item1 = document.getElementById('pic1');
+return item1;
+}
